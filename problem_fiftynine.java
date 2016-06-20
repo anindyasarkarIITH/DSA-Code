d67ad03ef521f63d2f41244e3e614c826c10ded5
anindyasarkarIITH <anindyasarkar.ece@gmail.com>
2016-06-21 03:31:11 +0530

    Add files via upload

---
+++ problem_fiftynine.java
@@ -0,0 +1,28 @@
+package bucky;
+import java.util.*;
+public class LinkList {
+	public void Method(LinkedList lv,int k){
+	LinkedList lt=new LinkedList<Integer>();
+	LinkedList result=new LinkedList<Integer>();
+	
+	for(int i=0;i<lv.size();i++){
+		if((int)lv.get(i)<k){
+			result.add(lv.get(i));
+			lv.remove(i);
+		}
+	}
+	for (int j=0;j<lv.size();j++){
+		result.add(lv.get(j));
+	}
+	for(int i=0;i<result.size();i++){
+		System.out.println(result.get(i));
+	}
+}
+	public static void main(String[] args){
+		LinkList ll=new LinkList();
+		LinkedList lt=new LinkedList<Integer>();
+		lt.add(1);lt.add(4);lt.add(3);lt.add(6);lt.add(5);lt.add(2);
+		
+		ll.Method(lt,3);
+	}
+}
